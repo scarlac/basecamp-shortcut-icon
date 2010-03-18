6a9dcb736f182d5947b49ee8dd0b49b9b48c7a3c
Seph Soliman <ss@konstellation.dk>
2010-03-19 00:14:35 +0100

    initial commit of version 1.1 (1.0 didnt function properly)

---
+++ src/basecamp-shortcut-icon.js
@@ -0,0 +1,6 @@
+// <link rel="shortcut icon" href="http://basecamphq.com/favicon.ico" type="image/x-icon" />
+var link = document.createElement('link');
+link.rel = 'shortcut icon';
+link.type = 'image/x-icon'
+link.href = 'http://basecamphq.com/favicon.ico';
+document.head.appendChild(link);
